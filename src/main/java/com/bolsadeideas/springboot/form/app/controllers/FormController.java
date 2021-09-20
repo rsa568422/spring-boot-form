d3a89f35e3995b80198700f60ef404ad2efeee29
Roberto Sánchez <rsa568422@gmail.com>
2021-09-20 22:48:35 +0200

    Clase 49:
    Se agrega la vista resultado.html.
    
    Se agrega titulo a form.html.
    
    Se agregan parametros al método procesar para recibir los datos del
    formulario al realizar el usuario un POST.

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -4,17 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
 
 	@GetMapping("/form")
 	public String form(Model model) {
+		model.addAttribute("title", "Formulario usuarios");
+		
 		return "form";
 	}
 
 	@PostMapping("/form")
-	public String procesar(Model model) {
+	public String procesar(	Model model,
+							@RequestParam String username,
+							@RequestParam String password,
+							@RequestParam String email) {
+		
+		model.addAttribute("title", "Resultado form");
+		model.addAttribute("username", username);
+		model.addAttribute("password", password);
+		model.addAttribute("email", email);
+		
 		return "resultado";
 	}
 
