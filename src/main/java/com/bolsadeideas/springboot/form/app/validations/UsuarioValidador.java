4fe832e83edc20b0fffb04165e091e4ec7e20b2e
Roberto Sánchez <rsa568422@gmail.com>
2021-09-22 23:24:57 +0200

    Clase 61:
    Se agrega la clase UsuarioValidador para la validación de objetos del
    tipo Usuario en formularios.
    
    Se agregan nuevos mensajes a messages.properties.
    
    Se elimina la etiqueta @Pattern del atributo identificador de la clase
    Usuario.
    
    Se agrega el atributo validador del tipo UsuarioValidador y se hace uso
    de él en el método procesar.

---
+++ src/main/java/com/bolsadeideas/springboot/form/app/validations/UsuarioValidador.java
@@ -0,0 +1,29 @@
+package com.bolsadeideas.springboot.form.app.validations;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
+
+@Component
+public class UsuarioValidador implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Usuario.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		Usuario usuario = (Usuario) target;
+
+		ValidationUtils.rejectIfEmpty(errors, "nombre", "NotEmpty.usuario.nombre");
+
+		if (!usuario.getIdentificador().matches("[\\d]{2}[.][\\d]{3}[.][\\d]{3}[-][A-Z]{1}")) {
+			errors.rejectValue("identificador", "pattern.usuario.identificador");
+		}
+	}
+
+}
