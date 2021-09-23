a35f60f6dd5b669495bf941389a80cd4286c972c
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2021-09-23 18:37:01 +0200

    Clase 67:
    Se cambia el tipo de input para el campo fechaNacimiento de text a date
    en la vista form.html.
    
    Se agrega la etiqueta @Past al atributo fechaNacimiento de la clase
    Usuario y se fija el formato como yyyy-MM-dd.
    
    NOTA: Los input de tipo date suelen enviar la fecha con un formato fijo yyyy-MM-dd, por lo que si vamos a emplearlos debemos utilizar ese mismo patrón para las fechas.

--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -41,7 +42,8 @@ public class Usuario {
 	private Integer cuenta;
 	
 	@NotNull
-	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	@Past
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date fechaNacimiento;
 
 	public String getIdentificador() {
