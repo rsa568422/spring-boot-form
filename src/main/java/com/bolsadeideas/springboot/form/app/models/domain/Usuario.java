3d5598c36b0eef6a8973ef5c9118bcdbb06063fc
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2021-09-23 18:24:15 +0200

    Clase 66:
    Se agrega el atributo fechaNacimiento a la clase Usuario y se le ponen
    las etiquetas @NotNull y @DateTimeFormat para indicar el patrón de la
    fecha tanto para la entrada como para la salida.
    
    Se agrega el campo con la fecha de nacimiento a las vistas form.html y
    resultado.html.
    
    Se agregan nuevos mensajes de error al fichero messages.properties.

--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,10 +1,14 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
+import java.util.Date;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.bolsadeideas.springboot.form.app.validations.labels.IdentificadorRegex;
 import com.bolsadeideas.springboot.form.app.validations.labels.Requerido;
@@ -35,6 +39,10 @@ public class Usuario {
 	@Min(5)
 	@Max(5000)
 	private Integer cuenta;
+	
+	@NotNull
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date fechaNacimiento;
 
 	public String getIdentificador() {
 		return identificador;
@@ -90,6 +98,14 @@ public class Usuario {
 
 	public void setCuenta(Integer cuenta) {
 		this.cuenta = cuenta;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
 	}
 
 }
