d2273f8cd658d8c7f326af79483b0b3293493638
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2021-09-24 14:21:31 +0200

    Clase 73:
    Refactorización de la vista form.html mostrando ahora la lista de países
    del atributo paisesMap mediante un Map<String, String>.
    
    Se agrega el método paisesMap a la clase FormController para el atributo
    paisesMap del modelo.

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -3,7 +3,9 @@ package com.bolsadeideas.springboot.form.app.controllers;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.validation.Valid;
 
@@ -46,6 +48,21 @@ public class FormController {
 	@ModelAttribute("paises")
 	public List<String> paises() {
 		return Arrays.asList("España", "México", "Chile", "Argentina", "Perú", "Colombia", "Venezuela");
+	}
+
+	@ModelAttribute("paisesMap")
+	public Map<String, String> paisesMap() {
+		Map<String, String> paises = new HashMap<>();
+
+		paises.put("ES", "España");
+		paises.put("MX", "México");
+		paises.put("CL", "Chile");
+		paises.put("AR", "Argentina");
+		paises.put("PE", "Perú");
+		paises.put("CO", "Colombia");
+		paises.put("VE", "Venezuela");
+
+		return paises;
 	}
 
 	@GetMapping("/form")
