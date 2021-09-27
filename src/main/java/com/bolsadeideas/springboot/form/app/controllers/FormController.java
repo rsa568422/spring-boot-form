1e0a079ba39833e4cdd68770de5e3766d6770482
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2021-09-27 12:44:13 +0200

    Clase 85:
    Se agrega el método ver a la clase FormController para redirigir a la
    vista resultado desde la url /ver y así reenviar la request y que si el
    usuario actualiza la página (F5), no se vuelva a enviar la request si no
    que se nos redirija a la vista form.html.

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
@@ -142,15 +143,24 @@ public class FormController {
 	}
 
 	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
-
-		model.addAttribute("title", "Resultado form");
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
 
 		if (result.hasErrors()) {
+			model.addAttribute("title", "Resultado form");
+			
 			return "form";
 		}
 
-		model.addAttribute("usuario", usuario);
+		return "redirect:/ver";
+	}
+	
+	@GetMapping("/ver")
+	public String ver(@SessionAttribute(name="usuario", required = false) Usuario usuario, Model model, SessionStatus status) {
+
+		if (usuario == null) return "redirect:/form";
+
+		model.addAttribute("title", "Resultado form");
+
 		status.setComplete();
 
 		return "resultado";
