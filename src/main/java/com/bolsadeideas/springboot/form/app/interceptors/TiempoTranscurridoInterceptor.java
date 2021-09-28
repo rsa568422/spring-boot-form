a4b83464384490cf217ec1d18e36c682bb25bc78
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2021-09-28 20:06:44 +0200

    Clase 91
    Se agrega log a los métodos de la clase TiempoTranscurridoInterceptor.
    
    Ahora el método preHandle agrega el atributo tiempoInicio a la vista y
    genera una espera aleatoria de [0..500)ms. El método postHandle recupera
    el valor de tiempoInicio y calcula el tiempo transcurrido, agregando el
    atriuto tiempoTranscurrido a la vista.

--- src/main/java/com/bolsadeideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -1,8 +1,12 @@
 package com.bolsadeideas.springboot.form.app.interceptors;
 
+import java.util.Random;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,17 +14,34 @@ import org.springframework.web.servlet.ModelAndView;
 @Component
 public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
 
+	private static final Logger logger = LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class);
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		logger.info("TiempoTranscurridoInterceptor: preHandle() entrando ...");
+		
+		request.setAttribute("tiempoInicio", System.currentTimeMillis());
+		
+		Random random = new Random();
+		Thread.sleep(random.nextInt(500));
+		
 		return true;
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
+		long tiempoFin = System.currentTimeMillis();
+		long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
+		long tiempoTranscurrido = tiempoFin - tiempoInicio;
+		
+		if (modelAndView != null) {
+			modelAndView.addObject("tiempoTranscurrido", tiempoTranscurrido);
+		}
 
-		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+		logger.info("Tiempo Transcurrido: " + tiempoTranscurrido + " milisegundos");
+		logger.info("TiempoTranscurridoInterceptor: preHandle() saliendo ...");
 	}
 
 }
