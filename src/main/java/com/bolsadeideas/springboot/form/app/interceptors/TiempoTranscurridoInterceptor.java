68316f3d9a33989e29420fa58880a1673668439b
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2021-09-28 19:22:22 +0200

    Clase 90
    Se agrega el paquete interceptors a la raíz del proyecto.
    
    Se agrega el componente TiempoTranscurridoInterceptor que implementa los
    métodos preHandle y postHandle del interface HandlerInterceptor.

---
+++ src/main/java/com/bolsadeideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -0,0 +1,26 @@
+package com.bolsadeideas.springboot.form.app.interceptors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+	}
+
+}
